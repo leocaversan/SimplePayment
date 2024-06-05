03941bfd38991ef5bd92311304bd8eaa9a3237fc
leoCaversan <leocaversan7@gmail.com>
2024-06-05 09:41:26 -0300

    fix: solve problem with network docker, kafka and Java app

--- simple-payment/src/main/java/br/com/simple_payment/notification/NotificationConsumer.java
+++ simple-payment/src/main/java/br/com/simple_payment/notification/NotificationConsumer.java
@@ -5,21 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationConsumer {
-    // private RestClient restClient;
 
-    // public NotificationConsumer(RestClient.Builder builder){
-    //     this.restClient = builder.baseUrl("").build();
-    // }
-
-    // @KafkaListener(topics = "transaction-notification", groupId = "simple_payment")
-    // public void receiveNotification(Transaction transaction){
-    //     var response = restClient.get()
-    //             .retrieve()
-    //             .toEntity(Notification.class);
-    //     if (response.getStatusCode().isError() || response.getBody().message())
-    //         throw new NotificationException("Error seding notification");
-
-    // }
     @KafkaListener(topics = "transaction-notification", groupId = "simple_payment")
     public void getMessage(String message){
         
